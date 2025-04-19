a9ab128d67a426b68f184b69d28dc64b68a76a4f
Cian <cian_mc_namara@hotmail.com>
2025-04-19 01:21:54 +0100

    Added summaries to ESoundType

--- DCPUtils/Enum/ESoundType.cs
+++ DCPUtils/Enum/ESoundType.cs
@@ -5,15 +5,45 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DCPUtils.Enum {
+    /// <summary>
+    /// All the different sound types that are supported in DCPs.
+    /// </summary>
     public enum ESoundType {
-        Mono,           // 10, center only
-        Stereo,         // 20, standard stereo
-        StereoCent,     // 21, stereo with center
-        StereoCentLFE,  // 31, stereo with center and LFE
-        Dolby51,        // 51, 5.1 surround
-        Dolby51HI,      // 61, 5.1 surround with hearing impairment track
-        Dolby71,        // 71, 7.1 surround
-        Dolby51HIViN,   // 911, 5.1 surround with hearing impairment/visual impairment tracks
-        Atmos           // 714, dolby atmos
+        /// <summary>
+        /// 10, center only
+        /// </summary>
+        Mono,
+        /// <summary>
+        /// 20, standard stereo
+        /// </summary>
+        Stereo,
+        /// <summary>
+        /// 21, stereo with center
+        /// </summary>
+        StereoCent,
+        /// <summary>
+        /// 31, stereo with center and LFE
+        /// </summary>
+        StereoCentLFE,
+        /// <summary>
+        /// 51, 5.1 surround
+        /// </summary>
+        Dolby51,
+        /// <summary>
+        /// 61, 5.1 surround with hearing impairment track
+        /// </summary>
+        Dolby51HI,
+        /// <summary>
+        /// 71, 7.1 surround
+        /// </summary>
+        Dolby71,
+        /// <summary>
+        /// 911, 5.1 surround with hearing impairment/visual impairment tracks
+        /// </summary>
+        Dolby51HIViN,
+        /// <summary>
+        /// 714, dolby atmos
+        /// </summary>
+        Atmos
     }
 }
