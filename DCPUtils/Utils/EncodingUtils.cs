cc4149fb25b78c3283b457d74b6aae4466e2d3a6
Cian <cian_mc_namara@hotmail.com>
2025-04-20 19:15:31 +0100

    [DCPUtils/Utils/EncodingUtils.cs] Added a summary to Base64Decode

--- DCPUtils/Utils/EncodingUtils.cs
+++ DCPUtils/Utils/EncodingUtils.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 
 namespace DCPUtils.Utils {
     public class EncodingUtils {
+        /// <summary>
+        /// Decodes a string from base 64 to a string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="convertHex"></param>
+        /// <returns></returns>
         public static string Base64Decode(string input, bool convertHex = true) {
             byte[] data = Convert.FromBase64String(input);
 
