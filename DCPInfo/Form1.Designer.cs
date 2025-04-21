debdd50d6a7fe65ac60e2f10e2b706aeccfae633
adrievx <177334560+adrievx@users.noreply.github.com>
2025-04-21 20:49:56 +0100

    [DCPInfo] Started testing DCP viewer

--- DCPInfo/Form1.Designer.cs
+++ DCPInfo/Form1.Designer.cs
@@ -23,13 +23,90 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.kryptonTextBox1 = new Krypton.Toolkit.KryptonTextBox();
+            this.kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            this.kryptonListBox1 = new Krypton.Toolkit.KryptonListBox();
+            this.kryptonTrackBar1 = new Krypton.Toolkit.KryptonTrackBar();
+            this.kryptonHeader1 = new Krypton.Toolkit.KryptonHeader();
+            this.kryptonGroupBox1 = new Krypton.Toolkit.KryptonGroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1.Panel)).BeginInit();
+            this.kryptonGroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // kryptonTextBox1
+            // 
+            this.kryptonTextBox1.Location = new System.Drawing.Point(352, 141);
+            this.kryptonTextBox1.Name = "kryptonTextBox1";
+            this.kryptonTextBox1.Size = new System.Drawing.Size(100, 23);
+            this.kryptonTextBox1.TabIndex = 0;
+            this.kryptonTextBox1.Text = "kryptonTextBox1";
+            // 
+            // kryptonLabel1
+            // 
+            this.kryptonLabel1.Location = new System.Drawing.Point(258, 142);
+            this.kryptonLabel1.Name = "kryptonLabel1";
+            this.kryptonLabel1.Size = new System.Drawing.Size(88, 20);
+            this.kryptonLabel1.TabIndex = 1;
+            this.kryptonLabel1.Values.Text = "kryptonLabel1";
+            // 
+            // kryptonListBox1
+            // 
+            this.kryptonListBox1.Location = new System.Drawing.Point(312, 205);
+            this.kryptonListBox1.Name = "kryptonListBox1";
+            this.kryptonListBox1.Size = new System.Drawing.Size(254, 141);
+            this.kryptonListBox1.TabIndex = 2;
+            // 
+            // kryptonTrackBar1
+            // 
+            this.kryptonTrackBar1.Location = new System.Drawing.Point(430, 76);
+            this.kryptonTrackBar1.Name = "kryptonTrackBar1";
+            this.kryptonTrackBar1.Size = new System.Drawing.Size(252, 27);
+            this.kryptonTrackBar1.TabIndex = 3;
+            // 
+            // kryptonHeader1
+            // 
+            this.kryptonHeader1.Location = new System.Drawing.Point(57, 52);
+            this.kryptonHeader1.Name = "kryptonHeader1";
+            this.kryptonHeader1.Size = new System.Drawing.Size(267, 36);
+            this.kryptonHeader1.TabIndex = 6;
+            this.kryptonHeader1.Values.Heading = "kryptonHeader1";
+            // 
+            // kryptonGroupBox1
+            // 
+            this.kryptonGroupBox1.Location = new System.Drawing.Point(82, 196);
+            this.kryptonGroupBox1.Size = new System.Drawing.Size(210, 150);
+            this.kryptonGroupBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kryptonGroupBox1);
+            this.Controls.Add(this.kryptonHeader1);
+            this.Controls.Add(this.kryptonTrackBar1);
+            this.Controls.Add(this.kryptonListBox1);
+            this.Controls.Add(this.kryptonLabel1);
+            this.Controls.Add(this.kryptonTextBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1.Panel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroupBox1)).EndInit();
+            this.kryptonGroupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Krypton.Toolkit.KryptonTextBox kryptonTextBox1;
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonListBox kryptonListBox1;
+        private Krypton.Toolkit.KryptonTrackBar kryptonTrackBar1;
+        private Krypton.Toolkit.KryptonHeader kryptonHeader1;
+        private Krypton.Toolkit.KryptonGroupBox kryptonGroupBox1;
     }
 }
 
